df858205a89855d7280464b312b8d80f6d1e4de8
Влад <v4nkov.v@yandex.ru>
2022-10-24 14:45:03 +0300

    main.html разбил на компоненты и установил связь между компонентами.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  dataMovies: Array<object>;
+};
+
+function App({dataMovies}: AppProps): JSX.Element {
+  return <MainPage dataMovies = {dataMovies} /> ;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const counter = 20;
+
+const getRandomInteger = (maxNumber: number) => {
+  const min = 0;
+  const max = Math.floor (maxNumber);
+  return Math.floor (Math.random () * (max - min + 1)) + min;
+};
+
+const images = [
+  'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  'img/bohemian-rhapsody.jpg',
+  'img/macbeth.jpg',
+  'img/aviator.jpg',
+  'img/we-need-to-talk-about-kevin.jpg'
+];
+const movieTitles = [
+  'Fantastic Beasts: The Crimes of Grindelwald',
+  'Bohemian Rhapsody',
+  'Macbeth',
+  'Aviator',
+  'We need to talk about Kevin'
+];
+
+const creatMovies = () => {
+  const movies = [];
+  for (let i = 0; i < counter; i++) {
+    const number = getRandomInteger (images.length - 1);
+    const data = {
+      image: images[number],
+      movieTitle: movieTitles[number]
+    };
+    movies.push (data);
+  }
+  return movies;
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App dataMovies = {creatMovies ()} />
   </React.StrictMode>,
 );
